6116beafd8386aa7dc4c9c758cce563a253f7020
Gyowanny P. Queiroz <81326247+gyohub@users.noreply.github.com>
2023-05-31 14:48:41 -0400

    Update JobEnqueuer.java

--- src/main/java/com/gyo/recollective/jobster/JobEnqueuer.java
+++ src/main/java/com/gyo/recollective/jobster/JobEnqueuer.java
@@ -4,7 +4,7 @@ import com.gyo.recollective.jobster.model.Job;
 
 /**
  * Interface that describes the contract of a job enqueuer implementation.
- * A job enqueuer must be capable of start itself, enqueue and shutdown itself.
+ * A job enqueuer must be capable of start itself, enqueue jobs and shutdown itself.
  */
 public interface JobEnqueuer {
     void enqueue(Job job);
